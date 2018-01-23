fa6180a7de9863878b3d3a8c3f36ef755ef0dd9e
Gabriel Berto <gabrielberto@icloud.com>
2018-01-23 13:45:47 -0200

    Fix build bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,6 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          `gatsby-remark-emoji`,
           {
             resolve:'gatsby-remark-images',
             options: {
